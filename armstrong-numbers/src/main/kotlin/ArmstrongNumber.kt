eb1eef25cca18869669479f6e62afd59df774611
teajar <haridasanrajit1@outlook.com>
2024-06-15 09:55:25 +0530

    "Rajit | add 2nd iteration for armstrong, using map{}"
    i think the fold approach might be faster, since in this map approach we
    are  using .sum() function at the end whihc iterates over it again, but
    the fold only iterates once.

--- armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
+++ armstrong-numbers/src/main/kotlin/ArmstrongNumber.kt
@@ -4,9 +4,9 @@ object ArmstrongNumber {
 
     fun check(input: Int): Boolean {
         val l = input.toString().count()
-        return input.toString().fold(0) {
-            acc,c -> acc.plus(c.digitToInt().toDouble().pow(l).toInt())
-        } == input
+        return input.toString().map {
+            it.digitToInt().toDouble().pow(l).toInt()
+        }.sum() == input
     }
 
 }
