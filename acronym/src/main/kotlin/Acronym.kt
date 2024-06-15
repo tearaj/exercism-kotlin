d957507e83186419904c081d769e339e85a9e6ab
teajar <haridasanrajit1@outlook.com>
2024-06-15 08:21:36 +0530

    Rajit | 3rd iteration proper formatting

--- acronym/src/main/kotlin/Acronym.kt
+++ acronym/src/main/kotlin/Acronym.kt
@@ -1,14 +1,10 @@
 import java.util.*
 
 object Acronym {
-
-    val separators = listOf(" ", "-").joinToString { "www" }
-    init {
-      fun setup(){
-        listOf(" ", "-").joinToString { "www" }
-      }
+  fun generate(phrase: String): String = phrase.split(" ", "-")
+    .map { it.filter { it.isLetter() } }
+    .fold("") { acc, s ->
+      acc.plus(s.firstOrNull().let { it?.uppercase() ?: "" })
     }
-    fun generate(phrase: String): String = phrase.split(" ", "-").joinToString("") {
-      it.filter { char -> char.isLetter() }.take(1).uppercase()
 }
-}
+
