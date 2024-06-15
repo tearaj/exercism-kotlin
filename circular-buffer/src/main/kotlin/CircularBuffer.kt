639a6d19723204fc7e623b04ce7582c1b6f09109
teajar <haridasanrajit1@outlook.com>
2024-06-15 12:05:36 +0530

    Rajit | first iteration, with syntactic sugar

--- circular-buffer/src/main/kotlin/CircularBuffer.kt
+++ circular-buffer/src/main/kotlin/CircularBuffer.kt
@@ -15,18 +15,15 @@ class CircularBuffer<T>(val capacity:Int) {
         else buffer.removeFirst()
 
 
-    fun write(value: T) {
-        if(buffer.size==capacity) throw BufferFullException()
+    fun write(value: T) =
+        if(buffer.size==capacity) throw BufferFullException() else
         buffer.addLast(value)
 
-    }
 
     fun overwrite(value: T) {
         if(buffer.size==capacity) buffer.removeFirst()
         buffer.addLast(value)
     }
 
-    fun clear() {
-        buffer.clear()
-    }
+    fun clear() = buffer.clear()
 }
